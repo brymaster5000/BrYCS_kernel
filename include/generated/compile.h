0e182ba477bae06d1265c916f8eca58e9517e429
bsmitty2 <bsmitty2@bsmitty2-HP-Compaq-6710b-RM342UT-ABA.(none)>
2013-06-08 19:45:32 -0400

    add sweep 2 wake - thanks bsmitty

---
+++ include/generated/compile.h
@@ -0,0 +1,7 @@
+/* This file is auto generated, version 11 */
+/*  PREEMPT */
+#define UTS_MACHINE "arm"
+#define UTS_VERSION "#11 PREEMPT Wed Jun 5 23:27:37 EDT 2013"
+#define LINUX_COMPILE_BY "brymaster5000"
+#define LINUX_COMPILE_HOST "bryslaptop"
+#define LINUX_COMPILER "gcc version 4.4.3 (GCC) "
